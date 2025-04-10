4680119a4a9828646fe221bc65d838673c663ce8
filipe-f-guilardi <filipefguilardi@gmail.com>
2025-04-10 16:47:12 -0300

    H2 configuration done

--- src/main/java/com/favero/todoapi/models/Todo.java
+++ src/main/java/com/favero/todoapi/models/Todo.java
@@ -1,4 +1,22 @@
 package com.favero.todoapi.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Todo {
+    public Long id;
+    public String title;
+    public String description;
+    public Boolean isCompleted;
+
+    public Todo(String title, String description, Boolean isCompleted) {
+        this.title = title;
+        this.description = description;
+        this.isCompleted = isCompleted;
+    }
 }
